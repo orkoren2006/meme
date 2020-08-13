8ea317b571e2c71d60896c5e491f502485ec5f00
orkoren2006 <68695873+orkoren2006@users.noreply.github.com>
2020-08-13 07:55:40 +0300

    Add files via upload

--- js/meme-controller.js
+++ js/meme-controller.js
@@ -2,8 +2,8 @@
 
 function init() {
     gCanvas = document.getElementById('myCanvas');
-    gCtx = gCanvas.getContext('2d'); 
-    
+    gCtx = gCanvas.getContext('2d');
+
     // window.addEventListener('resize', function(){
     //     resizeCanvas()
     // })
@@ -12,60 +12,110 @@ function init() {
 function openTab(evt, tabName) {
     // Declare all variables
     var i, tabcontent, tablinks;
-  
+
     // Get all elements with class="tabcontent" and hide them
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
+        tabcontent[i].style.display = "none";
     }
-  
+
     // Get all elements with class="tablinks" and remove the class "active"
     tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
-  
+
     // Show the current tab, and add an "active" class to the button that opened the tab
     document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
-  }
+}
 
 
-  function drawImg(num) {
+function drawImg(num, func) {
     const img = new Image();
     img.onload = () => {
         gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height);
+        func();
     }
     img.src = `img/${num}.jpg`;
-    openTab(event, 'generator')
+
 }
 
-function drawText(x, y) {
-    // gCtx.save();
-    gCtx.beginPath();
-    gCtx.lineWidth = '2';
-    gCtx.strokeStyle = 'red';
-    gCtx.fillStyle = 'white';
-    gCtx.font = '40px Impact';
-    gCtx.textAlign = 'center';
-    gCtx.fillText(gMeme.lines[0].txt, x, y);
-    gCtx.strokeText(gMeme.lines[0].txt, x, y);
-    // gCtx.restore();
+function onClickImage(num) {
+    openTab(event, 'generator')
+    drawImg(num, drawText)
+    gMeme.selectedImgId = num;
 }
+
+
+
+function drawText() {
+    
+    gCtx.lineWidth = '2';
+    gCtx.strokeStyle = gMeme.lines[index].color;
+    gCtx.fillStyle = 'white';
+    gCtx.font = `${gMeme.lines[index].size}px Impact`;
+    gCtx.textAlign = 'left';
+    gCtx.strokeText(gMeme.lines[index].txt, gMeme.lines[index].x, gMeme.lines[index].y);
+    gCtx.fillText(gMeme.lines[index].txt, gMeme.lines[index].x, gMeme.lines[index].y);
+    // ctx.fillText(stringTitle, 15, canvas.height / 2 + 35);
+}
+
+
 
 function insertText() {
-    // var text = document.querySelector('#txt').value;
-    //     document.querySelector('#live').innerHTML = text;
-    gMeme.lines[0].txt = document.querySelector('#txt').value; 
-    
-    drawText(x,y)
-    }
-    
-    function increaseFont() {
-    
-        gCtx.font = `${gMeme.lines[0].size++}px Impact`;
-    
-    }
+    gMeme.lines[index].txt = document.querySelector('#txt').value;
+    drawImg(gMeme.selectedImgId, drawText)
+}
+
+function increaseFont() {
+    gCtx.font = `${gMeme.lines[index].size++}px Impact`;
+    drawImg(gMeme.selectedImgId, drawText)
+}
+
+function decreaseFont() {
+    gCtx.font = `${gMeme.lines[index].size--}px Impact`;
+    drawImg(gMeme.selectedImgId, drawText)
+}
+
+function moveDown() {
+    gMeme.lines[index].y += 5;
+    drawImg(gMeme.selectedImgId, drawText)
+}
+
+function moveUp() {
+    gMeme.lines[index].y -= 5;
+    drawImg(gMeme.selectedImgId, drawText)
+}
+
+function alignLeft() {
+    gCtx.textAlign = 'right';
+    drawImg(gMeme.selectedImgId, drawText)
+}
+
+function addLine() {
+    index = 1;
+    document.querySelector('#txt').value = '';
+    drawImg(gMeme.selectedImgId, drawText)
+}
+
+
+function clearText() {
+    gMeme.lines[index].txt = '';
+    document.querySelector('#txt').value = '';
+    drawImg(gMeme.selectedImgId, drawText)
+}
+
+// function submitext() {
+//     gMeme.lines[index].txt = document.querySelector('#txt').value;
+//     document.querySelector('#txt').value = '';
+// }
+
+function downloadCanvas(elLink) {
+    const data = gCanvas.toDataURL();
+    elLink.href = data;
+    elLink.download = 'my-image.jpg';
+}
 
 function clearCanvas() {
     gCtx.clearRect(0, 0, gCanvas.width, gCanvas.height);
@@ -81,8 +131,3 @@ function clearCanvas() {
 //     gCanvas.height = elContainer.offsetHeight;
 // }
 
-function onIncreaseFont() {
-    increaseFont();
-  
-
-}
--- js/meme-service.js
+++ js/meme-service.js
@@ -3,25 +3,53 @@
 var gCanvas;
 var gCtx;
 
-var gKeywords = {'happy': 12,'funny puk': 1}
-var gImgs = [{id: 1, url: 'img/popo.jpg', keywords: ['happy']}];
+var index = 0;
+
 var gMeme = {
- selectedImgId: 5,
- selectedLineIdx: 0,
- lines: [
- {
- txt,
- size: 20,
- align: 'left',
- color: 'red'
- }
- ]
+    selectedImgId: 5,
+    selectedLineIdx: 0,
+    lines: [
+        {
+            txt: '',
+            size: 20,
+            align: 'left',
+            color: 'red',
+            x: 200,
+            y: 70
+        },
+        {
+            txt: '',
+            size: 20,
+            align: 'left',
+            color: 'blue',
+            x: 200,
+            y: 300
+        }
+    ]
 }
 
+var gKeywords = { 'happy': 12, 'funny puk': 1 }
 
-var x = 235;
-var y = 70;
-
+var gImgs = [
+    { id: 1, url: 'img/popo.jpg', keywords: ['happy'] },
+    { id: 2, url: 'img/popo.jpg', keywords: ['happy'] },
+    { id: 3, url: 'img/popo.jpg', keywords: ['happy'] },
+    { id: 4, url: 'img/popo.jpg', keywords: ['happy'] },
+    { id: 5, url: 'img/popo.jpg', keywords: ['happy'] },
+    { id: 6, url: 'img/popo.jpg', keywords: ['happy'] },
+    { id: 7, url: 'img/popo.jpg', keywords: ['happy'] },
+    { id: 8, url: 'img/popo.jpg', keywords: ['happy'] },
+    { id: 9, url: 'img/popo.jpg', keywords: ['happy'] },
+    { id: 10, url: 'img/popo.jpg', keywords: ['happy'] },
+    { id: 11, url: 'img/popo.jpg', keywords: ['happy'] },
+    { id: 12, url: 'img/popo.jpg', keywords: ['happy'] },
+    { id: 13, url: 'img/popo.jpg', keywords: ['happy'] },
+    { id: 14, url: 'img/popo.jpg', keywords: ['happy'] },
+    { id: 15, url: 'img/popo.jpg', keywords: ['happy'] },
+    { id: 16, url: 'img/popo.jpg', keywords: ['happy'] },
+    { id: 17, url: 'img/popo.jpg', keywords: ['happy'] },
+    { id: 18, url: 'img/popo.jpg', keywords: ['happy'] }
+];
 
 function checkCor(ev) {
     console.log(ev);
